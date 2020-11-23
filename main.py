fbb95f34f840577ceb5fd594def7abd8739c1899
laeeqah <laeeqahesau@gmail.com>
2020-11-23 15:01:04 +0200

    busy

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from tkinter import *
+import random
+import sys
 
 window = Tk()
 window.geometry("500x500")
-window.title("Lotto")
+window.title("Ithuba National Lottery of South Africa")
+window.configure(background = "maroon")
+
+
+lage = Label(window, text = "Your Age")
+lage.pack(padx = 0, pady = 0)
+
+lage_ent = Entry(window)
+lage_ent.pack(padx = 1, pady = 2)
+
+def choose():
+    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+random = Label(window, text = "Winner")
+random.pack()
+
+btn = Button(window, text = "Press", command = choose)
+
+
+
+
+window.mainloop()
